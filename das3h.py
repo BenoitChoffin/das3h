dc849ab944ec1a0e27fe65d68014b7ea58067eba
BenoitChoffin <benoit.choffin@hotmail.fr>
2020-07-08 15:16:37 +0200

    Add Assistments 2009 + grid search

--- das3h.py
+++ das3h.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import KFold
 from sklearn.metrics import roc_auc_score, accuracy_score, log_loss
 from sklearn.linear_model import LogisticRegression
 from scipy.sparse import load_npz, hstack, csr_matrix
+from collections import defaultdict
 import pandas as pd
 import pywFM
 import argparse
@@ -21,6 +22,7 @@ parser.add_argument('--dataset', type=str, nargs='?', default='assistments12')
 parser.add_argument('--generalization', type=str, nargs='?', default='strongest')
 parser.add_argument('--d', type=int, nargs='?')
 parser.add_argument('--C', type=float, nargs='?', default=1.)
+parser.add_argument('--grid_search', type=bool, nargs='?', const=True, default=False)
 parser.add_argument('--iter', type=int, nargs='?', default=300)
 parser.add_argument('--users', type=bool, nargs='?', const=True, default=False)
 parser.add_argument('--items', type=bool, nargs='?', const=True, default=False)
@@ -57,6 +59,13 @@ params = {
     'k2': options.d
 }
 
+if options.grid_search:
+	dict_of_auc = defaultdict(lambda: [])
+	dict_of_rmse = defaultdict(lambda: [])
+	dict_of_nll = defaultdict(lambda: [])
+	dict_of_acc = defaultdict(lambda: [])
+	list_of_elapsed_times = []
+
 for i, folds_file in enumerate(sorted(glob.glob(os.path.join(CSV_FOLDER, options.generalization, "folds/test_fold*.npy")))):
 	dataio.prepare_folder(os.path.join(EXPERIMENT_FOLDER, str(i)))
 	dt = time.time()
@@ -68,46 +77,95 @@ for i, folds_file in enumerate(sorted(glob.glob(os.path.join(CSV_FOLDER, options
 	X_test = X[test_ids,1:]
 	y_test = y[test_ids]
 
-	if options.d == 0:
-		print('fitting...')
-		estimators = [
-			('onehot', MaxAbsScaler()),
-			('lr', LogisticRegression(solver="saga", max_iter=options.iter, C=options.C))
-		]
-		pipe = Pipeline(estimators)
-		pipe.fit(X_train, y_train)
-		y_pred_test = pipe.predict_proba(X_test)[:, 1]
+	if options.grid_search:
+		if options.d == 0:
+			for c in [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10]:
+				print('fitting for c=...'.format(c))
+				estimators = [
+					('onehot', MaxAbsScaler()),
+					('lr', LogisticRegression(solver="saga", max_iter=options.iter, C=c))
+				]
+				pipe = Pipeline(estimators)
+				pipe.fit(X_train, y_train)
+				y_pred_test = pipe.predict_proba(X_test)[:, 1]
+				dict_of_auc[c].append(roc_auc_score(y_test, y_pred_test))
+				dict_of_rmse[c].append(np.sqrt(mean_squared_error(y_test, y_pred_test)))
+				dict_of_nll[c].append(log_loss(y_test, y_pred_test))
+				dict_of_acc[c].append(accuracy_score(y_test, np.round(y_pred_test)))
+			list_of_elapsed_times.append(np.around(time.time() - dt,3))
 	else:
-		fm = pywFM.FM(**params)
-		model = fm.run(X_train, y_train, X_test, y_test)
-		y_pred_test = np.array(model.predictions)
-		model.rlog.to_csv(os.path.join(EXPERIMENT_FOLDER, str(run_id), 'rlog.csv'))
-	
-	print(y_test)
-	print(y_pred_test)
-	ACC = accuracy_score(y_test, np.round(y_pred_test))
-	AUC = roc_auc_score(y_test, y_pred_test)
-	print('auc', AUC)
-	NLL = log_loss(y_test, y_pred_test)
-	print('nll', NLL)
-	RMSE = np.sqrt(mean_squared_error(y_test,y_pred_test))
+		if options.d == 0:
+			print('fitting...')
+			estimators = [
+				('onehot', MaxAbsScaler()),
+				('lr', LogisticRegression(solver="saga", max_iter=options.iter, C=options.C))
+			]
+			pipe = Pipeline(estimators)
+			pipe.fit(X_train, y_train)
+			y_pred_test = pipe.predict_proba(X_test)[:, 1]
+		else:
+			fm = pywFM.FM(**params)
+			model = fm.run(X_train, y_train, X_test, y_test)
+			y_pred_test = np.array(model.predictions)
+			model.rlog.to_csv(os.path.join(EXPERIMENT_FOLDER, str(i), 'rlog.csv'))
+		
+		print(y_test)
+		print(y_pred_test)
+		ACC = accuracy_score(y_test, np.round(y_pred_test))
+		AUC = roc_auc_score(y_test, y_pred_test)
+		print('auc', AUC)
+		NLL = log_loss(y_test, y_pred_test)
+		print('nll', NLL)
+		RMSE = np.sqrt(mean_squared_error(y_test,y_pred_test))
 
-	elapsed_time = np.around(time.time() - dt,3)
-	# Save experimental results
-	with open(os.path.join(EXPERIMENT_FOLDER, str(run_id), 'results.json'), 'w') as f:
-		f.write(json.dumps({
-			'date': str(today),
-			'args': experiment_args,
-			'legends': {
-				'short': short_legend,
-				'full': full_legend,
-				'latex': latex_legend
-			},
-			'metrics': {
-				'ACC': ACC,
-				'AUC': AUC,
-				'NLL': NLL,
-				'RMSE': RMSE
-			},
-			'elapsed_time': elapsed_time
-		}, indent=4))
+		elapsed_time = np.around(time.time() - dt,3)
+		# Save experimental results
+		with open(os.path.join(EXPERIMENT_FOLDER, str(i), 'results.json'), 'w') as f:
+			f.write(json.dumps({
+				'date': str(today),
+				'args': experiment_args,
+				'legends': {
+					'short': short_legend,
+					'full': full_legend,
+					'latex': latex_legend
+				},
+				'metrics': {
+					'ACC': ACC,
+					'AUC': AUC,
+					'NLL': NLL,
+					'RMSE': RMSE
+				},
+				'elapsed_time': elapsed_time
+			}, indent=4))
+
+if options.grid_search:
+	list_of_hp = []
+	list_of_mean_metrics = []
+	for c in dict_of_auc.keys():
+		list_of_hp.append(c)
+		list_of_mean_metrics.append(np.mean(dict_of_auc[c]))
+	optimal_hp = list_of_hp[np.argmax(list_of_mean_metrics)]
+	print("Optimal set of HP found: {}".format(optimal_hp))
+	print("Overall AUC : {}".format(np.around(np.mean(dict_of_auc[optimal_hp]),3)))
+	print("Overall RMSE : {}".format(np.around(np.mean(dict_of_rmse[optimal_hp]),3)))
+	print("Overall NLL : {}".format(np.around(np.mean(dict_of_nll[optimal_hp]),3)))
+
+	for i in range(len(list_of_elapsed_times)):
+		with open(os.path.join(EXPERIMENT_FOLDER, str(i), 'results.json'), 'w') as f:
+			f.write(json.dumps({
+				'date': str(today),
+				'args': experiment_args,
+				'legends': {
+					'short': short_legend,
+					'full': full_legend,
+					'latex': latex_legend
+				},
+				'metrics': {
+					'ACC': dict_of_acc[optimal_hp][i],
+					'AUC': dict_of_auc[optimal_hp][i],
+					'NLL': dict_of_nll[optimal_hp][i],
+					'RMSE': dict_of_rmse[optimal_hp][i]
+				},
+				'elapsed_time': list_of_elapsed_times[i],
+				'optimal_hp': optimal_hp
+			}, indent=4))
--- prepare_data.py
+++ prepare_data.py
@@ -9,10 +9,11 @@ parser = argparse.ArgumentParser(description='Prepare datasets.')
 parser.add_argument('--dataset', type=str, nargs='?', default='assistments12')
 parser.add_argument('--min_interactions', type=int, nargs='?', default=10)
 parser.add_argument('--remove_nan_skills', type=bool, nargs='?', const=True, default=False)
+parser.add_argument('--verbose', type=bool, nargs='?', const=True, default=False)
 options = parser.parse_args()
 
 
-def prepare_assistments12(min_interactions_per_user, remove_nan_skills):
+def prepare_assistments12(min_interactions_per_user, remove_nan_skills, verbose):
 	"""Preprocess ASSISTments 2012-2013 dataset.
 
 	Arguments:
@@ -24,49 +25,168 @@ def prepare_assistments12(min_interactions_per_user, remove_nan_skills):
 	Q_mat -- corresponding q-matrix (item-skill relationships sparse array)
 	"""
 	df = pd.read_csv("data/assistments12/data.csv")
-	initial_shape = df.shape[0]
-	print("Opened ASSISTments 2012 data. Output: {} samples.".format(initial_shape))
+	if verbose:
+		initial_shape = df.shape[0]
+		print("Opened ASSISTments 2012 data. Output: {} samples.".format(initial_shape))
 	
 	df["timestamp"] = df["start_time"]
 	df["timestamp"] = pd.to_datetime(df["timestamp"])
 	df["timestamp"] = df["timestamp"] - df["timestamp"].min()
 	df["timestamp"] = df["timestamp"].apply(lambda x: x.total_seconds()).astype(np.int64)
-	df.sort_values(by="timestamp", inplace=True)
-	df.reset_index(inplace=True, drop=True)
-	df = df.groupby("user_id").filter(lambda x: len(x) >= min_interactions_per_user)
-
+	#df.sort_values(by="timestamp", inplace=True)
+	#df.reset_index(inplace=True, drop=True)
 	if remove_nan_skills:
 		df = df[~df["skill_id"].isnull()]
+		if verbose:
+			print("Removed {} samples with NaN skills.".format(df.shape[0]-initial_shape))
+			initial_shape = df.shape[0]
 	else:
-		df.ix[df["skill_id"].isnull(), "skill_id"] = -1
+		df.loc[df["skill_id"].isnull(), "skill_id"] = -1
+
+	df = df[df.correct.isin([0,1])] # Remove potential continuous outcomes
+	if verbose:
+		print("Removed {} samples with non-binary outcomes.".format(df.shape[0]-initial_shape))
+		initial_shape = df.shape[0]
+	df['correct'] = df['correct'].astype(np.int32) # Cast outcome as int32
+
+	df = df.groupby("user_id").filter(lambda x: len(x) >= min_interactions_per_user)
+	if verbose:
+		print('Removed {} samples (users with less than {} interactions).'.format((df.shape[0]-initial_shape,
+																				   min_interactions_per_user)))
+		initial_shape = df.shape[0]
 
 	df["user_id"] = np.unique(df["user_id"], return_inverse=True)[1]
 	df["item_id"] = np.unique(df["problem_id"], return_inverse=True)[1]
 	df["skill_id"] = np.unique(df["skill_id"], return_inverse=True)[1]
 	
-	df.reset_index(inplace=True, drop=True) # Add unique identifier of the row
-	df["inter_id"] = df.index
+	#df.reset_index(inplace=True, drop=True) # Add unique identifier of the row
+	#df["inter_id"] = df.index
 
 	# Build Q-matrix
 	Q_mat = np.zeros((len(df["item_id"].unique()), len(df["skill_id"].unique())))
 	item_skill = np.array(df[["item_id", "skill_id"]])
 	for i in range(len(item_skill)):
 		Q_mat[item_skill[i,0],item_skill[i,1]] = 1
+	if verbose:
+		print("Computed q-matrix. Shape: {}.".format(Q_mat.shape))
 
-	df = df[['user_id', 'item_id', 'timestamp', 'correct', "inter_id"]]
-	df = df[df.correct.isin([0,1])] # Remove potential continuous outcomes
-	df['correct'] = df['correct'].astype(np.int32) # Cast outcome as int32
-	print("Data preprocessing done. Removed {} interactions. Final output: {} samples.".format((df.shape[0]-initial_shape,
-																								df.shape[0])))
+	#df = df[['user_id', 'item_id', 'timestamp', 'correct', "inter_id"]]
+	df = df[['user_id', 'item_id', 'timestamp', 'correct']]
+	# Remove potential duplicates
+	df.drop_duplicates(inplace=True)
+	if verbose:
+		print("Removed {} duplicated samples.".format(df.shape[0] - initial_shape))
+		initial_shape = df.shape[0]
 
+	df.sort_values(by="timestamp", inplace=True)
+	df.reset_index(inplace=True, drop=True)
+	print("Data preprocessing done. Final output: {} samples.".format((df.shape[0])))
 	# Save data
 	sparse.save_npz("data/assistments12/q_mat.npz", sparse.csr_matrix(Q_mat))
 	df.to_csv("data/assistments12/preprocessed_data.csv", index=False)
 
 	return df, Q_mat
 
+def prepare_assistments09(min_interactions_per_user, remove_nan_skills, verbose):
+	"""Preprocess ASSISTments 2009-2010 dataset.
+	Requires the collapsed version: skill_builder_data_corrected_collapsed.csv
+	Download it on: https://sites.google.com/site/assistmentsdata/home/assistment-2009-2010-data/skill-builder-data-2009-2010 (the last link)
+
+	Actually thanks to the ASSISTments team, we had access to another file,
+	timestamp_data.csv, that contains the timestamps.
+	This extra file does not seem openly available yet.
+
+	Arguments:
+	min_interactions_per_user -- minimum number of interactions per student
+	remove_nan_skills -- if True, remove interactions with no skill tag
+
+	Outputs:
+	df -- preprocessed ASSISTments dataset (pandas DataFrame)
+	Q_mat -- corresponding q-matrix (item-skill relationships sparse array)
+	"""
+
+	df = pd.read_csv("data/assistments09/skill_builder_data_corrected_collapsed.csv",
+		encoding = "latin1", index_col=False)
+	df.drop(['Unnamed: 0'], axis=1, inplace=True)
+	if verbose:
+		initial_shape = df.shape[0]
+		print("Opened ASSISTments 2009 data. Output: {} samples.".format(initial_shape))
+	timestamps = pd.read_csv("data/assistments09/timestamp_data.csv")
+
+	df = df.merge(timestamps, left_on="order_id", right_on="problem_log_id", how="inner")
+	df["timestamp"] = df["start_time"]
+	df["timestamp"] = pd.to_datetime(df["timestamp"])
+	df["timestamp"] = df["timestamp"] - df["timestamp"].min()
+	df["timestamp"] = df["timestamp"].apply(lambda x: x.total_seconds()).astype(np.int64)
+	#df.sort_values(by="timestamp", inplace=True)
+	#df.reset_index(inplace=True, drop=True)
+
+	# Remove NaN skills
+	if remove_nan_skills:
+		initial_shape = df.shape[0] # in case the merge above removed some samples
+		df = df[~df["skill_id"].isnull()]
+		if verbose:
+			print("Removed {} samples with NaN skills.".format(df.shape[0]-initial_shape))
+			initial_shape = df.shape[0]
+	else:
+		df.loc[df["skill_id"].isnull(), "skill_id"] = -1
+
+	df = df[df.correct.isin([0,1])] # Remove potential continuous outcomes
+	if verbose:
+		print("Removed {} samples with non-binary outcomes.".format(df.shape[0]-initial_shape))
+		initial_shape = df.shape[0]
+	df['correct'] = df['correct'].astype(np.int32) # Cast outcome as int32
+
+	df = df.groupby("user_id").filter(lambda x: len(x) >= min_interactions_per_user)
+	if verbose:
+		print('Removed {} samples (users with less than {} interactions).'.format((df.shape[0]-initial_shape,
+																				   min_interactions_per_user)))
+		initial_shape = df.shape[0]
+
+	df["item_id"] = np.unique(df["problem_id"], return_inverse=True)[1]
+	df["user_id"] = np.unique(df["user_id"], return_inverse=True)[1]
+
+	# Build q-matrix
+	listOfKC = []
+	for kc_raw in df["skill_id"].unique():
+		for elt in str(kc_raw).split('_'):
+			listOfKC.append(str(int(float(elt))))
+	listOfKC = np.unique(listOfKC)
+
+	dict1_kc = {} ; dict2_kc = {}
+	for k, v in enumerate(listOfKC):
+		dict1_kc[v] = k
+		dict2_kc[k] = v
+
+	# Build Q-matrix
+	Q_mat = np.zeros((len(df["item_id"].unique()), len(listOfKC)))
+	item_skill = np.array(df[["item_id","skill_id"]])
+	for i in range(len(item_skill)):
+		splitted_kc = str(item_skill[i,1]).split('_')
+		for kc in splitted_kc:
+			Q_mat[item_skill[i,0],dict1_kc[str(int(float(kc)))]] = 1
+	if verbose:
+		print("Computed q-matrix. Shape: {}.".format(Q_mat.shape))
+
+	df = df[['user_id', 'item_id', 'timestamp', 'correct']]
+	# Remove potential duplicates
+	df.drop_duplicates(inplace=True)
+	if verbose:
+		print("Removed {} duplicated samples.".format(df.shape[0] - initial_shape))
+		initial_shape = df.shape[0]
+
+	df.sort_values(by="timestamp", inplace=True)
+	df.reset_index(inplace=True, drop=True)
+	print("Data preprocessing done. Final output: {} samples.".format((df.shape[0])))
+
+	# Save data
+	sparse.save_npz("data/assistments09/q_mat.npz", sparse.csr_matrix(Q_mat))
+	df.to_csv("data/assistments09/preprocessed_data.csv", index=False)
+
+	return df, Q_mat
+
 def prepare_kddcup10(data_name, min_interactions_per_user, kc_col_name,
-					 remove_nan_skills, drop_duplicates=True):
+					 remove_nan_skills, verbose, drop_duplicates=True):
 	"""Preprocess KDD Cup 2010 datasets.
 
 	Arguments:
@@ -89,26 +209,46 @@ def prepare_kddcup10(data_name, min_interactions_per_user, kc_col_name,
 		'First Transaction Time': 'timestamp',
 		'Correct First Attempt': 'correct'
 	})[['user_id', 'pb_id', 'step_id' ,'correct', 'timestamp', 'kc_id']]
-	initial_shape = df.shape[0]
-	print("Opened KDD Cup data. Output: {} samples.".format(initial_shape))
+	if verbose:
+		initial_shape = df.shape[0]
+		print("Opened KDD Cup 2010 data. Output: {} samples.".format(initial_shape))
+
 	df["timestamp"] = pd.to_datetime(df["timestamp"])
 	df["timestamp"] = df["timestamp"] - df["timestamp"].min()
 	df["timestamp"] = df["timestamp"].apply(lambda x: x.total_seconds()).astype(np.int64)
-	df.sort_values(by="timestamp",inplace=True)
-	df.reset_index(inplace=True,drop=True)
+	#df.sort_values(by="timestamp",inplace=True)
+	#df.reset_index(inplace=True,drop=True)
+
+	if remove_nan_skills:
+		df = df[~df["kc_id"].isnull()]
+		if verbose:
+			print("Removed {} samples with NaN skills.".format(df.shape[0]-initial_shape))
+			initial_shape = df.shape[0]
+	else:
+		df.loc[df["kc_id"].isnull(), "kc_id"] = 'NaN'
+
+	df = df[df.correct.isin([0,1])] # Remove potential continuous outcomes
+	if verbose:
+		print("Removed {} samples with non-binary outcomes.".format(df.shape[0]-initial_shape))
+		initial_shape = df.shape[0]
+	df['correct'] = df['correct'].astype(np.int32) # Cast outcome as int32
+
 	df = df.groupby("user_id").filter(lambda x: len(x) >= min_interactions_per_user)
+	if verbose:
+		print('Removed {} samples (users with less than {} interactions).'.format((df.shape[0]-initial_shape,
+																				   min_interactions_per_user)))
+		initial_shape = df.shape[0]
 
 	# Create variables
 	df["item_id"] = df["pb_id"]+":"+df["step_id"]
 	df = df[['user_id', 'item_id', 'kc_id', 'correct', 'timestamp']]
 
-	if drop_duplicates:
-		df.drop_duplicates(subset=["user_id", "item_id", "timestamp"], inplace=True)
-	
-	if remove_nan_skills:
-		df = df[~df["kc_id"].isnull()]
-	else:
-		df.ix[df["kc_id"].isnull(), "kc_id"] = 'NaN'
+	# Transform ids into numeric
+	df["item_id"] = np.unique(df["item_id"], return_inverse=True)[1]
+	df["user_id"] = np.unique(df["user_id"], return_inverse=True)[1]
+
+	#if drop_duplicates:
+	#	df.drop_duplicates(subset=["user_id", "item_id", "timestamp"], inplace=True)
 
 	# Create list of KCs
 	listOfKC = []
@@ -123,12 +263,8 @@ def prepare_kddcup10(data_name, min_interactions_per_user, kc_col_name,
 		dict1_kc[v] = k
 		dict2_kc[k] = v
 
-	# Transform ids into numeric
-	df["item_id"] = np.unique(df["item_id"], return_inverse=True)[1]
-	df["user_id"] = np.unique(df["user_id"], return_inverse=True)[1]
-
-	df.reset_index(inplace=True, drop=True) # Add unique identifier of the row
-	df["inter_id"] = df.index
+	#df.reset_index(inplace=True, drop=True) # Add unique identifier of the row
+	#df["inter_id"] = df.index
 
 	# Build Q-matrix
 	Q_mat = np.zeros((len(df["item_id"].unique()), len(listOfKC)))
@@ -137,12 +273,20 @@ def prepare_kddcup10(data_name, min_interactions_per_user, kc_col_name,
 		splitted_kc = item_skill[i,1].split('~~')
 		for kc in splitted_kc:
 			Q_mat[item_skill[i,0],dict1_kc[kc]] = 1
+	if verbose:
+		print("Computed q-matrix. Shape: {}.".format(Q_mat.shape))
 
-	df = df[['user_id', 'item_id', 'timestamp', 'correct', 'inter_id']]
-	df = df[df.correct.isin([0,1])] # Remove potential continuous outcomes
-	df['correct'] = df['correct'].astype(np.int32) # Cast outcome as int32
-	print("Data preprocessing done. Removed {} interactions. Final output: {} samples.".format((df.shape[0]-initial_shape,
-																								df.shape[0])))
+	#df = df[['user_id', 'item_id', 'timestamp', 'correct', 'inter_id']]
+	df = df[['user_id', 'item_id', 'timestamp', 'correct']]
+	# Remove potential duplicates
+	df.drop_duplicates(inplace=True)
+	if verbose:
+		print("Removed {} duplicated samples.".format(df.shape[0] - initial_shape))
+		initial_shape = df.shape[0]
+	
+	df.sort_values(by="timestamp", inplace=True)
+	df.reset_index(inplace=True, drop=True)
+	print("Data preprocessing done. Final output: {} samples.".format((df.shape[0])))
 	
 	# Save data
 	sparse.save_npz(folder_path + "/q_mat.npz", sparse.csr_matrix(Q_mat))
@@ -150,8 +294,9 @@ def prepare_kddcup10(data_name, min_interactions_per_user, kc_col_name,
 
 	return df, Q_mat
 
-def prepare_robomission(min_interactions_per_user):
+def prepare_robomission(min_interactions_per_user, verbose):
 	"""Preprocess Robomission dataset.
+	Retrieved from https://github.com/adaptive-learning/adaptive-learning-research/tree/master/data/robomission-2019-12
 
 	Arguments:
 	min_interactions_per_user -- minimum number of interactions per student
@@ -162,28 +307,41 @@ def prepare_robomission(min_interactions_per_user):
 	"""
 
 	df = pd.read_csv("data/robomission/attempts.csv") # from robomission-2019-12-10
-	initial_shape = df.shape[0]
-	print("Opened Robomission data. Output: {} samples.".format(initial_shape))
+	if verbose:
+		initial_shape = df.shape[0]
+		print("Opened Robomission data. Output: {} samples.".format(initial_shape))
 
 	df["correct"] = df["solved"].astype(np.int32)
 	df["timestamp"] = df["start"]
 	df["timestamp"] = pd.to_datetime(df["timestamp"])
 	df["timestamp"] = df["timestamp"] - df["timestamp"].min()
 	df["timestamp"] = df["timestamp"].apply(lambda x: x.total_seconds()).astype(np.int64)
-	df.sort_values(by="timestamp",inplace=True)
-	df.reset_index(inplace=True,drop=True)
+	#df.sort_values(by="timestamp",inplace=True)
+	#df.reset_index(inplace=True,drop=True)
 	df = df.groupby("student").filter(lambda x: len(x) >= options.min_interactions)
+	if verbose:
+		print('Removed {} samples (users with less than {} interactions).'.format((df.shape[0]-initial_shape,
+																				   min_interactions_per_user)))
+		initial_shape = df.shape[0]
 
 	# Change user/item identifiers
 	df["user_id"] = np.unique(df["student"], return_inverse=True)[1]
-	df["item_id"] = np.unique(df["item_id"], return_inverse=True)[1]
+	df["item_id"] = np.unique(df["problem"], return_inverse=True)[1]
 
-	df.reset_index(inplace=True, drop=True) # Add unique identifier of the row
-	df["inter_id"] = df.index
+	#df.reset_index(inplace=True, drop=True) # Add unique identifier of the row
+	#df["inter_id"] = df.index
 
-	df = df[['user_id', 'item_id', 'timestamp', 'correct', "inter_id"]]
-	print("Data preprocessing done. Removed {} interactions. Final output: {} samples.".format((df.shape[0]-initial_shape,
-																								df.shape[0])))
+	#df = df[['user_id', 'item_id', 'timestamp', 'correct', "inter_id"]]
+	df = df[['user_id', 'item_id', 'timestamp', 'correct']]
+	# Remove potential duplicates
+	df.drop_duplicates(inplace=True)
+	if verbose:
+		print("Removed {} duplicated samples.".format(df.shape[0] - initial_shape))
+		initial_shape = df.shape[0]
+	
+	df.sort_values(by="timestamp",inplace=True)
+	df.reset_index(inplace=True, drop=True)
+	print("Data preprocessing done. Final output: {} samples.".format((df.shape[0])))
 
 	# Sort q-matrix by item id
 	Q_mat = pd.read_csv("data/robomission/qmatrix.csv")
@@ -199,16 +357,24 @@ def prepare_robomission(min_interactions_per_user):
 if __name__ == "__main__":
 	if options.dataset == "assistments12":
 		df, Q_mat = prepare_assistments12(min_interactions_per_user=options.min_interactions,
-										  remove_nan_skills=options.remove_nan_skills)
+										  remove_nan_skills=options.remove_nan_skills,
+										  verbose=options.verbose)
+	if options.dataset == "asssistments09":
+		df, Q_mat = prepare_assistments09(min_interactions_per_user=options.min_interactions,
+										  remove_nan_skills=options.remove_nan_skills,
+										  verbose=options.verbose)
 	elif options.dataset == "bridge_algebra06":
 		df, Q_mat = prepare_kddcup10(data_name="bridge_algebra06",
 									 min_interactions_per_user=options.min_interactions,
 									 kc_col_name="KC(SubSkills)",
-									 remove_nan_skills=options.remove_nan_skills)
+									 remove_nan_skills=options.remove_nan_skills,
+									 verbose=options.verbose)
 	elif options.dataset == "algebra05":
 		df, Q_mat = prepare_kddcup10(data_name="algebra05",
 									 min_interactions_per_user=options.min_interactions,
 									 kc_col_name="KC(Default)",
-									 remove_nan_skills=options.remove_nan_skills)
+									 remove_nan_skills=options.remove_nan_skills,
+									 verbose=options.verbose)
 	elif options.dataset == "robomission":
-		df, Q_mat = prepare_robomission(min_interactions_per_user=options.min_interactions
+		df, Q_mat = prepare_robomission(min_interactions_per_user=options.min_interactions,
+										verbose=options.verbose)
